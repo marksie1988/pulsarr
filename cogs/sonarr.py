3d9a8a4489463314b3c0765973b49fee6e9ca838
marksie1988 <marksie1988@users.noreply.github.com>
2020-04-16 22:17:44 +0100

    Update sonarr.py

--- cogs/sonarr.py
+++ cogs/sonarr.py
@@ -38,27 +38,50 @@ class Sonarr(commands.Cog):
             stautsEmbed.add_field(name='Status', value='In Production', inline=False)
         stautsEmbed.add_field(name='Debug Enabled', value=debug, inline=False)
         stautsEmbed.set_footer(text=f'Sonarr Version: {ver}')
-        print(sysStatus)
+
         await ctx.send(embed=stautsEmbed)
 
     @commands.command()
     async def diskSpace(self, ctx):
         """- Show diskspace of your Sonarr instance."""
-        with sonarr.get_diskspace() as diskSpace:
-            for disk in diskSpace:
-                print(disk)
-                path = disk['path']
-                space = disk['freespace']
-                total = disk['totalspace']
+        diskSpace = sonarr.get_diskspace()
 
-                diskEmbed = discord.Embed(
-                    title = f'{path} Usage',
-                    description = '',
-                    colour = discord.Colour.blue()
-                )
-                diskEmbed.add_field(name='Free Space', value=space, inline=False)
-                diskEmbed.add_field(name='Total Space', value=total, inline=False)
-                await ctx.send(embed=diskEmbed)
+        def GetHumanReadable(size,precision=2):
+            suffixes=['B','KB','MB','GB','TB']
+            suffixIndex = 0
+            while size > 1024 and suffixIndex < 4:
+                suffixIndex += 1 #increment the index of the suffix
+                size = size/1024.0 #apply the division
+            return "%.*f%s"%(precision,size,suffixes[suffixIndex])
+
+        embedColour = discord.Colour.green()
+
+        for disk in diskSpace:
+            freePct = (disk['freeSpace'] / disk['totalSpace'] * 100)
+            if freePct < 10:
+                embedColour = discord.Colour.red()
+
+        diskEmbed = discord.Embed(
+            title = 'Disk Usage',
+            description = '',
+            colour = embedColour
+        )
+
+        for disk in diskSpace:
+            path = disk['path']
+            space = GetHumanReadable(disk['freeSpace'])
+            total = GetHumanReadable(disk['totalSpace'])
+            diskEmbed.add_field(name='Path', value=path, inline=True)
+            diskEmbed.add_field(name='Free Space', value=space, inline=True)
+            diskEmbed.add_field(name='Total Space', value=total, inline=True)
+            await ctx.send(embed=diskEmbed)
+
+    @commands.command()
+    async def seriesLookup(self, ctx, term):
+        """- Lookup a series based on a search term or tvdb:<seriesId>."""
+        
+        lookup = sonarr.lookup_series(term)
+        print(lookup)
 
 def setup(bot):
     bot.add_cog(Sonarr(bot))
