9929725fdcba4ca82286ebe9afe522bd27aa77d1
Steven Marks <Steven.Marks@rackspace.com>
2020-04-16 17:45:41 +0100

    update sonarr commands

--- cogs/sonarr.py
+++ cogs/sonarr.py
@@ -1,7 +1,7 @@
 
 import discord, yaml, requests
 from discord.ext import commands
-from arr import SonarrAPI
+from PyArr import SonarrAPI
 
 with open("config.yml", "r") as ymlfile:
     try:
@@ -25,10 +25,40 @@ class Sonarr(commands.Cog):
 
     @commands.command()
     async def status(self, ctx):
-        """- Show tatus of your Sonarr instance."""
-        sysStatus = sonarr.get_system_status
+        """- Show status of your Sonarr instance."""
+        sysStatus = sonarr.get_system_status()
+        stautsEmbed = discord.Embed(
+            title = 'Sonarr Status',
+            description = '',
+            colour = discord.Colour.blue()
+        )
+        ver = sysStatus['version']
+        debug = sysStatus['isDebug']
+        if sysStatus['isProduction'] == True: 
+            stautsEmbed.add_field(name='Status', value='In Production', inline=False)
+        stautsEmbed.add_field(name='Debug Enabled', value=debug, inline=False)
+        stautsEmbed.set_footer(text=f'Sonarr Version: {ver}')
+        print(sysStatus)
+        await ctx.send(embed=stautsEmbed)
 
-        await ctx.send()
+    @commands.command()
+    async def diskSpace(self, ctx):
+        """- Show diskspace of your Sonarr instance."""
+        with sonarr.get_diskspace() as diskSpace:
+            for disk in diskSpace:
+                print(disk)
+                path = disk['path']
+                space = disk['freespace']
+                total = disk['totalspace']
+
+                diskEmbed = discord.Embed(
+                    title = f'{path} Usage',
+                    description = '',
+                    colour = discord.Colour.blue()
+                )
+                diskEmbed.add_field(name='Free Space', value=space, inline=False)
+                diskEmbed.add_field(name='Total Space', value=total, inline=False)
+                await ctx.send(embed=diskEmbed)
 
 def setup(bot):
     bot.add_cog(Sonarr(bot))
